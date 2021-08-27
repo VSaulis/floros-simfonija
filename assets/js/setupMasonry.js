ba5fffae055a1489e8a1b53113fbb6954f151149
Vytautas Saulis <vaulis11@gmail.com>
2021-08-27 18:09:46 +0300

    Changes

--- assets/js/setupMasonry.js
+++
@@ -1,9 +0,0 @@
-import Masonry from 'masonry-layout'
-
-export const setupMasonry = () => {
-  const masonry = new Masonry('.masonry', {
-    itemSelector: '.masonry-item',
-  });
-
-  masonry.layout();
-};

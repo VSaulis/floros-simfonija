17d3b74c44d8c84e5fb586cd513c5c71b94e09cc
Vytautas Saulis <vaulis11@gmail.com>
2021-09-05 01:14:47 +0300

    Changes

--- assets/js/setupMap.js
+++ assets/js/setupMap.js
@@ -1,14 +1,16 @@
 export const setupMap = () => {
-  const container = document.getElementById('map');
-  const longitude = container.getAttribute('data-longitude');
-  const latitude = container.getAttribute('data-latitude');
+  window.onload = () => {
+    const container = document.getElementById('map');
+    const longitude = container.getAttribute('data-longitude');
+    const latitude = container.getAttribute('data-latitude');
 
-  const uluru = { lat: Number(latitude), lng: Number(longitude) };
+    const uluru = {lat: Number(latitude), lng: Number(longitude)};
 
-  const map = new google.maps.Map(container, { zoom: 18, center: uluru });
+    const map = new google.maps.Map(container, {zoom: 18, center: uluru, disableDefaultUI: true});
 
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  };
 };
